6bc7dc5d32ff519f189c086eff80a4a4c6bfb3be
Ma2z0un <shoubar@hotmail.com>
2021-08-14 02:29:12 +0300

    html course

---
+++ js.js
@@ -0,0 +1,9 @@
+  
+//     if(location.href=="https://blog.codershiyar.com/" || location.href=="http://blog.codershiyar.com/?m=1" || location.href=="http://blog.codershiyar.com/" ||  location.href=="https://blog.codershiyar.com/" || location.href=="https://blog.codershiyar.com/?m=1"){
+//           document.getElementById("website2").style.display = "none";
+//         document.getElementById("website").style.display = "block";
+      
+//   }else{
+//          document.getElementById("website2").style.display = "block";
+//       document.getElementById("website").style.display = "none";
+//   }
